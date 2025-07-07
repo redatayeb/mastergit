b546dcffd7e090c4fabb5342836da220a7498246
REDA Tayeb <Ta.Reda@emsi-edu.ma>
2025-07-07 13:21:25 +0100

    Revert "add files"
    This reverts commit 1f32eebdd6129279b9e14171857079ad78644bd5.

--- Models/User.cs
+++ Models/User.cs
@@ -2,13 +2,5 @@
 {
     public class User
     {
-        public int MyProperty { get; set; }
-        public string Name { get; set; }
-
-        public string Email { get; set; }
-
-        public string Password { get; set; }
-
-        public User() { }
     }
 }
